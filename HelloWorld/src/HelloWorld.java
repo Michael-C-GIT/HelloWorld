ae1397d5e35eb47ab10dc6ee657a6d48fd0c6862
Michael Chen <chen0536@algonquinlive.com>
2020-11-29 05:21:32 -0500

    added a HelloWorld.java file

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Student Name: Michael Chen (Student Number: 040533117)
+ * Lab Section: CST8116 | Section 332 | Introduction to Computer Programming
+ * Lab Professor: Professor Cyr Michel Bakinde
+ * Due Date: December 1, 2020
+ * Modified: November 29, 2020
+ * Description: Hello World with GIT
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
